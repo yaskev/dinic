0c33515bb4575b03b12b8bfe8e0407662fae0a66
yaskev <yaskevich.san@mail.ru>
2019-05-23 22:19:31 +0300

    Full code

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# dinic
-MaxFlow MinCut Problem using Dinic algorithm
+# Max Flow Min Cut (Dinic algorithm)
+### Description
+This is the implementation of the Max Flow Min Cut problem (To find the maximum flow in a given graph from Source to Sink)
+
+The algorithm implemented: Dinic
---
+++ cmake-build-debug/CMakeFiles/MaxFlowMinCut.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MaxFlowMinCut.dir/main.cpp.obj"
+  "MaxFlowMinCut.pdb"
+  "MaxFlowMinCut.exe"
+  "MaxFlowMinCut.exe.manifest"
+  "libMaxFlowMinCut.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MaxFlowMinCut.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
